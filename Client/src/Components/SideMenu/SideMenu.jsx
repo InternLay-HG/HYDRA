7e941d8e98bc3237cf6b2c1a5d82264cd8e76a24
Ayush Singhal <ayushsinghal304@gmail.com>
2024-11-09 14:26:19 +0530

    sidebar

--- Client/src/Components/SideMenu/SideMenu.jsx
+++ Client/src/Components/SideMenu/SideMenu.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import './sidemenu.css'
 
 function SideMenu() {
   return (
-    <div>SideMenu</div>
+    <>
+
+<div className="sidebar">
+
+        <div className="logo"> <img id="icon" src="./Images/logo.png" alt="" /></div>
+      <div className="highlight-rectangle"></div>
+      <div className="sidebar-icons">
+        <div className="icon">
+            <img id='mySpace' src="./Images/Subtract.png" alt="" />
+            <span className="tooltip">My Space</span>
+        </div>
+        <div className="icon">
+            <img id='mySpace' src="./Images/Subtract2.png" alt="" />
+            <span className="tooltip">Selected Galaxy</span>
+        </div>
+        <div className="icon">
+            <img id='mySpace' src="./Images/Subtract3.png" alt="" />
+            <span className="tooltip">Confession</span>
+        </div>
+        <div className="icon">
+            <img id='mySpace' src="./Images/Subtract4.png" alt="" />
+            <span className="tooltip">Resource Hub</span>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
