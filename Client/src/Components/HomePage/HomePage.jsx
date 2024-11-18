973a53b0379e3d5ddc7c752235f5e5dab927bf23
Ayush Singhal <ayushsinghal304@gmail.com>
2024-11-19 02:43:41 +0530

    HomePage changes

--- Client/src/Components/HomePage/HomePage.jsx
+++ Client/src/Components/HomePage/HomePage.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './HomePage.css'
 
 function HomePage() {
   return (
-    <div>hello</div>
+    <>
+    <div className="OUTER">
+      <div className="feedarea">
+        <img id = 'banner' src="/Images/Banner.png" alt="" />
+        <div id="frame52">
+          <p className='first'>Featured Galaxies</p>
+          <p className='second'>See All</p>
+        </div>
+        <div id="frame53">
+          <p className='first'>Popular Galaxies</p>
+          <p className='second'>See All</p>
+        </div>
+        <div id="frame54">
+          <p className='first'>Recently Added</p>
+          <p className='second'>See All</p>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
