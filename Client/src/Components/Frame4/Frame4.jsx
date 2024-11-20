339dc46f9777100828420a270b2fa2b4558b9c4b
Ayush Singhal <ayushsinghal304@gmail.com>
2024-11-20 20:38:14 +0530

    LoginPage2

--- Client/src/Components/Frame4/Frame4.jsx
+++ Client/src/Components/Frame4/Frame4.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Frame4.css'
 
 function Frame4() {
   return (
-    <div>Frame Lorem, ipsum dolor.</div>
+    <>
+      <div className="OUTER2">
+        <div id="frame59">
+          <div id="frame43">
+            <img src="/Images/notification.png" alt="" id='notifictaion' />
+            <img src="/Images/profile.png" alt="" id='profile'/>
+            <img src="/Images/settings.png" alt="" id='settings'/>
+          </div>
+          <img src="/Images/Ellipse 14.png" alt="" id='userphoto'/>
+          <div id="name">HIMANSHU</div>
+          <div id="nameid">@astrohimanshu</div>
+        </div>
+      </div>
+    </>
   )
 }
 
